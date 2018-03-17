87048cf1166e21adf9c017559032653ddd60f166
Radostin Ivanov <radipleven@abv.bg>
2018-03-17 17:46:48 +0200

    Added comments to Beverage_Labels.cs

--- intro-and-basic-syntax/exercise/Beverage_Labels.cs
+++ intro-and-basic-syntax/exercise/Beverage_Labels.cs
@@ -6,16 +6,16 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            string product_name = Console.ReadLine();
-            int volume = int.Parse(Console.ReadLine());
-            int energy_per_100ml = int.Parse(Console.ReadLine());
-            int sugar_per_100ml = int.Parse(Console.ReadLine());
+            string product_name = Console.ReadLine(); //user inputs the name of the product
+            int volume = int.Parse(Console.ReadLine());// user inputs the amount of the product
+            int energy_per_100ml = int.Parse(Console.ReadLine());// user inputs the energy per 100ml the product has
+            int sugar_per_100ml = int.Parse(Console.ReadLine());//user inputs the amount of sugar per 100ml the product has
 
             //double total_energy = ((double)volume / 100.0) * energy_per_100ml;
             //double total_sugars = ((double)volume / 100.0) * sugar_per_100ml;
 
-            Console.WriteLine($"{volume}ml {product_name}:");
-            Console.WriteLine($"{((double)volume / 100.0) * energy_per_100ml}kcal, {((double)volume / 100.0) * sugar_per_100ml}g sugars");
+            Console.WriteLine($"{volume}ml {product_name}:"); // console outputs the volume and the name of the product
+            Console.WriteLine($"{((double)volume / 100.0) * energy_per_100ml}kcal, {((double)volume / 100.0) * sugar_per_100ml}g sugars");// console outputs the calculations that have been made
 
             //Console.ReadLine();
         }
