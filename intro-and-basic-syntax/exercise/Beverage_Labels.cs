35017c63553b74e24737d1052d29aa4439c7b883
Radostin Ivanov <radipleven@abv.bg>
2018-03-17 17:53:49 +0200

    Added comments to Beverage_Labels.cs

--- intro-and-basic-syntax/exercise/Beverage_Labels.cs
+++ intro-and-basic-syntax/exercise/Beverage_Labels.cs
@@ -11,13 +11,12 @@ namespace ConsoleApp2
             int energy_per_100ml = int.Parse(Console.ReadLine());//user inputs the energy per 100ml the product has
             int sugar_per_100ml = int.Parse(Console.ReadLine());//user inputs the amount of sugar per 100ml the product has
 
-            //double total_energy = ((double)volume / 100.0) * energy_per_100ml;
-            //double total_sugars = ((double)volume / 100.0) * sugar_per_100ml;
+            
 
             Console.WriteLine($"{volume}ml {product_name}:"); // console outputs the volume and the name of the product
             Console.WriteLine($"{((double)volume / 100.0) * energy_per_100ml}kcal, {((double)volume / 100.0) * sugar_per_100ml}g sugars");// console outputs the calculations that have been made
 
-            //Console.ReadLine();
+           
         }
     }
 }
