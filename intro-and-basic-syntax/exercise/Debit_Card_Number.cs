e687bf3d020919d87e18212125bb569f2d3e8626
TheChill <kristiandimitrov97@gmail.com>
2018-03-17 16:54:23 +0200

    Added 1 problem "Debit_Card_Number"

---
+++ intro-and-basic-syntax/exercise/Debit_Card_Number.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Debit_Card_Number
+    {
+        static void Main()
+        {
+            string debit_card_number = "";
+            for (int i = 0; i < 4; i++)
+            {
+                string four_digits = Console.ReadLine();
+
+                debit_card_number += new String('0', 4 - four_digits.Length) + four_digits;
+
+                if (i < 3)
+                {
+                    debit_card_number += " ";
+                }
+            }
+            
+            Console.WriteLine(debit_card_number);
+
+            //Console.ReadLine();
+        }
+    }
+}
