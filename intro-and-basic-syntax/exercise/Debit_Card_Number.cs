56aeb1a3b482f333e8f6857b80dc5d447f04e71d
stoian515 <stoian515@abv.bg>
2018-03-17 17:35:42 +0200

    Comment on Problem 1

--- intro-and-basic-syntax/exercise/Debit_Card_Number.cs
+++ intro-and-basic-syntax/exercise/Debit_Card_Number.cs
@@ -6,19 +6,21 @@ namespace ConsoleApp1
     {
         static void Main()
         {
+	    // create empty string
             string debit_card_number = "";
             for (int i = 0; i < 4; i++)
             {
+		// for each i we read 4 digits
                 string four_digits = Console.ReadLine();
-
+		// adding the digits to the string
                 debit_card_number += new String('0', 4 - four_digits.Length) + four_digits;
-
+		// addin the last digits
                 if (i < 3)
                 {
                     debit_card_number += " ";
                 }
             }
-            
+	    // print the result            
             Console.WriteLine(debit_card_number);
 
             //Console.ReadLine();
