d34c72167bdc0dc117268e265eaacfa89acc63c3
stoian515 <stoian515@abv.bg>
2018-03-17 17:46:16 +0200

    Comments on Problem 3

--- intro-and-basic-syntax/exercise/Miles_To_Kilometers.cs
+++ intro-and-basic-syntax/exercise/Miles_To_Kilometers.cs
@@ -12,13 +12,13 @@ namespace MilesToKilometers
         {
 
 
-
+	    // we read the miles
             double inputMiles = double.Parse(Console.ReadLine());
 
-
+	    // converting the miles into kilometers
             double calculatedKilometers = inputMiles * 1.60934;
 
-
+ 	    // printing the kilometers (f2)
             Console.WriteLine($"{calculatedKilometers:f2}");
         }
     }
