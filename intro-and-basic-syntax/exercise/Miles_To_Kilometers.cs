29897b813963dfdb363b6eb688b24c77ff53b163
Radostin Ivanov <radipleven@abv.bg>
2018-03-17 17:39:07 +0200

    Added Miles_To_Kilometers.cs

---
+++ intro-and-basic-syntax/exercise/Miles_To_Kilometers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilesToKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            double inputMiles = double.Parse(Console.ReadLine());
+
+
+            double calculatedKilometers = inputMiles * 1.60934;
+
+
+            Console.WriteLine($"{calculatedKilometers:f2}");
+        }
+    }
+}
