150a51dcfd2195e3c524b6c0179d7958089025b4
TheChill <kristiandimitrov97@gmail.com>
2018-03-17 17:23:36 +0200

    Added problem 4 "Beverage_Labels"

---
+++ intro-and-basic-syntax/exercise/Beverage_Labels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Beverage_Labels
+    {
+        static void Main(string[] args)
+        {
+            string product_name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energy_per_100ml = int.Parse(Console.ReadLine());
+            int sugar_per_100ml = int.Parse(Console.ReadLine());
+
+            //double total_energy = ((double)volume / 100.0) * energy_per_100ml;
+            //double total_sugars = ((double)volume / 100.0) * sugar_per_100ml;
+
+            Console.WriteLine($"{volume}ml {product_name}:");
+            Console.WriteLine($"{((double)volume / 100.0) * energy_per_100ml}kcal, {((double)volume / 100.0) * sugar_per_100ml}g sugars");
+
+            //Console.ReadLine();
+        }
+    }
+}
